de7bcb1a2e661e7fe01cef58ac0181136e1c6d92
BeomSeogKim <kbs4520@daum.net>
2023-10-20 01:52:45 +0900

    Initial Commit

---
+++ CodeDeploy/src/test/java/com/tommy/codedeploy/CodeDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.codedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
