f974019f710848a7e1b6f22491ab4d2b34b5f21a
BeomSeogKim <kbs4520@daum.net>
2023-10-21 01:16:25 +0900

    Health Check 추가

---
+++ src/main/java/com/tommy/codedeploy/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.tommy.codedeploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "WAS IS RUNNING";
+    }
+}
